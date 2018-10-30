6c10e0ebd565c6e98371361f66b70c4695d9c94e
Kace Cottam <kaceac1@hotmail.com>
2018-10-30 14:55:51 -0700

    message

--- ListBot.py
+++ ListBot.py
@@ -6,10 +6,9 @@ import asyncio
 import re
 
 client = discord.Client()
-dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = os.path.dirname(os.path.realpath(__file__))+'/Content/'
 @client.event
 async def on_ready():
-    dir_path = os.path.dirname(os.path.realpath(__file__))+'/Content'
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
@@ -17,20 +16,16 @@ async def on_ready():
     if not os.path.exists(dir_path):
         print('Content folder does not exist. Creating...')
         os.mkdir(dir_path)
-    dir_path+='/'
-
-#args = message.content.slice(prefix.length).trim().split(/ +/g);
-#command = args.shift().shift().toLowerCase();
-
-#re.compile('!add \"(.*)\" list \"(.*)\"|!add list (?:\"(\w+)\")')
+    print('dir_path: '+dir_path)
 
 @client.event
 async def on_message(message):
+    print(message.content)
     if message.content.startswith('!add list'):
         print('Trying to make a list...')
         matchedContent = re.compile('!add list (?:\"(\w+)\")').match(message.content)
         if matchedContent is not None:
-            if os.path.exists(dir_path+matchedContent[1]):
+            if  os.path.exists(dir_path+matchedContent[1]) is not False:
                 print('Command Failed: List already exists.')
                 await client.send_message(message.channel, 'List already exists: '+matchedContent[1])
             else:
@@ -41,8 +36,6 @@ async def on_message(message):
             print('Command Failed: Bad Syntax')
             await client.send_message(message.channel, 'Try again.')
             await client.send_message(message.channel, 'The command syntax is "!add list "<list name>"')
-
-
     elif message.content.startswith('!add'):
         print('Trying to add to a list...')
         matchedContent = re.compile('!add \"(.*)\" \"(.*)\"').match(message.content)
@@ -61,8 +54,5 @@ async def on_message(message):
             print('Command failed: Bad Syntax')
             await client.send_message(message.channel, 'Try again.')
             await client.send_message(message.channel, 'The command syntax is "!add "<list>" "<item>"')
-    print('\n')
-    
-
 
 client.run('NDkxNjk0MzA0MTI2MDQyMTEy.DrogKg.PEATf9YqFIMVNEj1c6PE8dqtTKY')
