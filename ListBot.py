af194ab0a4f54cc274b5bcae950513447166237b
KaceCottam <kace.cottam@wsu.edu>
2019-01-18 13:36:07 -0800

    Itemization

--- ListBot.py
+++ ListBot.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import sys
 import asyncio
+import shutil
 
 client = discord.Client()
 dir_path = os.path.dirname(os.path.realpath(__file__))+'/Content/'
@@ -21,20 +22,10 @@ def AddFolder(path):
     if not CheckFolder(path):
         os.mkdir(path)
 
-def AddItem(path):
-    if not CheckFolder(path):
-        return
-    f=open(path,'w+')
-    f.close()
-
-def RemoveItem(path):
-    RemoveFolder(path)
-    return
-
 @client.event
 async def on_ready():
     print('Linux Program TREE is required.')
-    print('Logged in as' + client.user.name + ' ' + client.user.id+'')
+    print('Logged in as ' + client.user.name + ' ' + str(client.user.id)+'')
     print('------')
     if not CheckFolder(dir_path):
         print('Content folder does not exist. Creating...')
@@ -43,73 +34,62 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     content = message.content.lower().split()
     print(content)
     if content[0] == '!add':
-        if 'list' in content:
+        if 'item' in content:
             curPath = dir_path
+            messageCurPath = ""
             i = 1
-            while content[i] != 'list':
+            while content[i] != 'item':
                 if not CheckFolder(curPath):
-                    await client.send_message(message.channel, 'ERROR: There is no list called '+ content[i] +'!')
+                    await message.channel.send('ERROR: There is no item called '+ content[i+1] +'!')
                     return
                 curPath += '/'+content[i]
+                messageCurPath += content[i]+'/'
                 i += 1
-            AddFolder(curPath+'/'+content[len(content)-1])
-            await client.send_message(message.channel, 'INFO: Created a list called '+content[i]+'!')
+            AddFolder(curPath+'/'+content[i+1])
+            await message.channel.send('INFO: Created a item called '+messageCurPath+content[i+1]+'!')
             return
         else:
-            curPath = dir_path
-            i = 1
-            while i != len(content) - 2:
-                if not CheckFolder(curPath):
-                    await client.send_message(message.channel, 'ERROR: There is no list called '+ content[i] +'!')
-                    return
-                curPath += content[i]
-                i += 1
-            AddItem(curPath+'/'+content[len(content)-1])
+            await message.channel.send('ERROR: Bad syntax! You must use \"item\" in your command.')
             return
         return
     elif content[0] == '!remove':
-        if 'list' in content:
+        if 'item' in content:
             curPath = dir_path
+            messageCurPath = ""
             i = 1
-            while content[i] != 'list':
+            while content[i] != 'item':
                 if not CheckFolder(curPath):
-                    await client.send_message(message.channel, 'ERROR: There is no list called '+ content[i] +'!')
+                    await message.channel.send('ERROR: There is no item called '+ content[i+1] +'!')
                     return
                 curPath += '/'+content[i]
+                messageCurPath += content[i]+'/'
                 i += 1
-            RemoveFolder(curPath+'/'+content[len(content)-1])
-            await client.send_message(message.channel, 'INFO: Removed a list called '+content[len(content_=1)]+'!')
+            RemoveFolder(curPath+'/'+content[i+1])
+            await message.channel.send('INFO: Removed a item called '+messageCurPath+content[i+1]+'!')
             return
         else:
-            curPath = dir_path
-            i = 1
-            while i != len(content) - 2:
-                if not CheckFolder(curPath):
-                    await client.send_message(message.channel, 'ERROR: There is no list called '+ content[i] +'!')
-                    return
-                curPath += content[i]
-                i += 1
-            RemoveItem(curPath+'/'+content[len(content)-1])
+            await message.channel.send('ERROR: Bad syntax! You must use \"item\" in your command.')
             return
         return
-    elif message.content.startswith('!list'):
-        print('Displaying Lists...')
-        os.system("tree -n -F -a Content | sed 's/├\|─\|│\|└/ /g' > lists")
-        await client.send_message(message.channel, 'Here is all of the lists.')
-        f = open("lists","r")
+    elif content[0] == '!item':
+        os.system("tree -n -F -a Content | sed 's/├\|─\|│\|└/ /g' > items")
+        await message.channel.send('Here is all of the items.')
+        f = open("items","r")
         line = f.readline()
         while line:
-            await client.send_message(message.channel, '_ _ '+line)
+            await message.channel.send('_ _ '+line)
             print(line)
             line = f.readline()
             if line == '\n':
                 line = None
         f.close()
-        os.remove(os.path.dirname(os.path.realpath(__file__))+'/lists')
-        await client.send_message(message.channel, 'End of Content.')        
+        os.remove(os.path.dirname(os.path.realpath(__file__))+'/items')
+        await message.channel.send('End of Content.')        
         print('End Command\n')
         
 f=open("token.txt","r")
